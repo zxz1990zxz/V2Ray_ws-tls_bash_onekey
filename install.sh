ac2b9a6f6d0d336d35de54346cb6b4c92ee56533
zxz1990zxz <74815337+zxz1990zxz@users.noreply.github.com>
2023-11-05 00:10:40 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -433,7 +433,7 @@ ssl_install() {
     fi
     judge "安装 SSL 证书生成脚本依赖"
 
-    curl https://get.acme.sh | sh
+    curl https://get.acme.sh | sh -s email=admin@zxz1990zxz@163.com
     judge "安装 SSL 证书生成脚本"
 }
 
